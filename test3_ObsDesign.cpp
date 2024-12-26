67411e975d1e4138d554ed65bd354927eb580366
Abel Tom <abeltom.kernel@gmail.com>
2024-12-26 10:51:26 +0100

    Added random temperature generator. Bit of documentation. Changed Interface name Added car head unit.

--- test3_ObsDesign.cpp
+++ test3_ObsDesign.cpp
@@ -1,28 +1,34 @@
+#include <chrono>
 #include <iostream>
+#include <random>
+#include <unistd.h>
 #include <vector>
 
-class DisplayUnit {
+// Interface for Observer: Display Unit observes
+class IDisplayUnit {
   std::string mName;
 
 public:
   virtual void notify(int16_t temperature){};
   virtual std::string printName() const = 0;
 
-  virtual ~DisplayUnit(){};
+  virtual ~IDisplayUnit(){};
 };
 
-class ConsoleDisplay : public DisplayUnit {
+class ConsoleDisplay : public IDisplayUnit {
   std::string mConsoleName;
 
 public:
   ConsoleDisplay(const std::string name) { mConsoleName = name; }
+
+  // Notify method called from Observer.
   void notify(int16_t temperature) override {
     std::cout << "Temperature updated: " << temperature << "°C" << std::endl;
   }
   std::string printName() const override { return mConsoleName; }
 };
 
-class CarHeadUnit : public DisplayUnit {
+class CarHeadUnit : public IDisplayUnit {
   std::string mConsoleName;
   std::string mCarName;
 
@@ -38,17 +44,27 @@ public:
   std::string carName() const { return mCarName; }
 };
 
+// Subject: notifies observers as soon as something changes
+// TODO: use smart pointers instead of raw pointers.
 class WeatherStation {
-  std::vector<DisplayUnit *> mObservers;
+  std::vector<IDisplayUnit *> mObservers;
   std::int16_t mTemperature;
 
 public:
   void setTemperature(std::int16_t temperature) {
     mTemperature = temperature;
+    auto start = std::chrono::high_resolution_clock::now();
     notify_observers();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    // To get the value of duration use the count()
+    // member function on the duration object
+    std::cout << duration.count() << " us." << std::endl;
   }
 
-  void registerObserver(DisplayUnit *dispUnit) {
+  void registerObserver(IDisplayUnit *dispUnit) {
     mObservers.push_back(dispUnit);
   }
 
@@ -66,15 +82,24 @@ public:
 int main() {
 
   ConsoleDisplay disp1("Living room"), disp2("Kitchen");
-  CarHeadUnit disp3("Nissan");
+  CarHeadUnit disp3("Nissan"), disp4("Toyota");
 
   WeatherStation station;
 
   station.registerObserver(&disp1);
   station.registerObserver(&disp2);
   station.registerObserver(&disp3);
+  station.registerObserver(&disp4);
 
-  station.setTemperature(55);
+  // Generate random temperatures from -55 to +55 C.s
+  std::random_device rd;
+  std::mt19937 e2(rd());
+  std::uniform_int_distribution<int> dist(-55, 55);
 
+  for (auto i = 0; i <= 50; i++) {
+    auto curr_temperature = dist(e2);
+    station.setTemperature(curr_temperature);
+    usleep(200000);
+  }
   return 0;
 }
