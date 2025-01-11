34cdfad59255ce038ac6ea0a4a8054edb9d34197
Abel Thomas <abel@abel-amd5700>
2025-01-11 10:38:57 +0100

    Used ASIO threads:
    - used a different IP to read more data,
    - used an idle worker thread.

--- test-boost-example/test_boost.cpp
+++ test-boost-example/test_boost.cpp
@@ -21,14 +21,41 @@ int boost_regex() {
   return 0;
 }
 
+std::vector<char> vBuffer(1 * 1024);
+
+void grabSomeData(boost::asio::ip::tcp::socket &socket) {
+
+  socket.async_read_some(boost::asio::buffer(vBuffer.data(), vBuffer.size()),
+                         [&](std::error_code ec, std::size_t len) {
+                           if (!ec) {
+                             std::cout << "\nRead: " << len << "bytes"
+                                       << std::endl;
+                             for (size_t i = 0; i < len; i++)
+                               std::cout << vBuffer[i];
+
+                           } else {
+                           }
+                           grabSomeData(socket);
+                         });
+}
+
 int main() {
 
   constexpr const char *ipAddress = IP_ADDR;
 
   boost::system::error_code ec;
 
+  // Create a context
   boost::asio::io_context context;
 
+  // Fake tasks context, "idle task"
+  // Use executor_work_guard to keep the  io_context running
+  auto idleWork = boost::asio::make_work_guard(context);
+
+  // Start context
+  std::thread thrContext = std::thread([&]() { context.run(); });
+
+  // create an endpoint
   boost::asio::ip::tcp::endpoint end_pt(
       boost::asio::ip::make_address_v4(ipAddress, ec), PORT);
 
@@ -47,6 +74,7 @@ int main() {
 
   if (socket.is_open()) {
 
+    grabSomeData(socket);
     std::string sRequest = "GET /index.html HTTP/1.1\r\n"
                            "HOST: example.com\r\n"
                            "Connection: close\r\n\r\n";
@@ -54,24 +82,12 @@ int main() {
     socket.write_some(boost::asio::buffer(sRequest.data(), sRequest.size()),
                       ec);
 
-    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    socket.wait(socket.wait_read);
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(2000ms);
 
-    std::size_t bytes = socket.available();
-
-    std::cout << "Bytes available: " << bytes << std::endl;
-
-    if (bytes > 0) {
-
-      std::vector<char> vBuff(bytes);
-      socket.read_some(boost::asio::buffer(vBuff.data(), vBuff.size()), ec);
-
-      std::cout << "Err mess on reading socket: " << ec.message() << std::endl;
-
-      for (auto byte : vBuff) {
-        std::cout << byte;
-      }
-    }
+    context.stop();
+    if (thrContext.joinable())
+      thrContext.join();
   }
 
   return 0;
